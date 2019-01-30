4e728c57f9249719434acd6307a06c636b78204d
Mitchel Parish <mitchelparish@gmail.com>
2019-01-30 22:50:12 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,9 @@
 class MP3Importer
-  attr_accessor :path
+  attr_reader :file
 
-   def initialize(path)
-    @path = path
-  end
+  def initialize(name)
+    @name = name
+  end 
 
-   def files
-    Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
-  end
 
-   def import
-    files.each { |file| Song.new_by_filename(file) }
-  end
-end 
+end
