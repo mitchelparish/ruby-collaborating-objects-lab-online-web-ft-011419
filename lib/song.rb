4e722daa7f07d5083fded1e9889fd66d212d0cdd
Mitchel Parish <mitchelparish@gmail.com>
2019-01-30 23:53:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,8 @@ class Song
     artist_and_song = file_name.split(" - ")
     artist = Artist.new(artist_and_song[0])
     song = self.new(artist_and_song[1])
-    song.artist = artist.save 
+    song.artist = artist.save
+    artist.add_song(song)
     song
   end
 end
