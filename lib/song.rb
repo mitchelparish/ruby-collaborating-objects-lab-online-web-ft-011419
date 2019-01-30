edd5736003b0a15d95632534ab9e85e978f0f525
Mitchel Parish <mitchelparish@gmail.com>
2019-01-30 22:48:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,16 @@ require 'pry'
 
 class Song
   @@all = []
-  attr_accessor :name, :artist 
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
-  def self.new_by_filename(file_name) 
+  def self.new_by_filename(file_name)
     binding.pry 
   end
-  
+
 
 
 
