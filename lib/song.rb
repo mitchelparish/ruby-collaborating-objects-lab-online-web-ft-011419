80a0b55145b41ec23a62788c1964fc3ff9148cc6
Mitchel Parish <mitchelparish@gmail.com>
2019-01-30 22:40:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,10 @@
 class Song
-  attr_accessor :name, :artist
+  @@all = []
+  attr_reader :name 
 
   def initialize(name)
     @name = name
-  end
 
-  def self.new_by_filename(filename)
-    artist_name, song_name, extra = filename.split(" - ")
-    song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(song)
-  end
+
+
 end
