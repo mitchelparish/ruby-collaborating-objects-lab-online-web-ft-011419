b7299b9c461820e300f55c6240223ad694cc9d81
Mitchel Parish <mitchelparish@gmail.com>
2019-01-30 23:36:05 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,7 +2,14 @@ class MP3Importer
   attr_reader :file
 
   def initialize(path)
-    @path = path 
+    @path = path
+  end
+
+  def files
+    Dir.chdir(@path) do
+      Dir.glob('*.mp3')
+    end
+
   end
 
 
